19d77f59bd38f5e454069967aa52cd852ab834a0
Ali <100485408+ixsalimo@users.noreply.github.com>
2025-01-25 11:12:00 +0330

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,23 @@
-document.querySelector("input[type='file']").addEventListener("change" , function (e) {
-    document.getElementById("profile").src = URL.createObjectURL(e.target.files[0]);
-    document.getElementById("file-name").textContent = e.target.files[0].name;
-});
-document.querySelector("button[type='reset']").addEventListener("click" , function () {
-    document.getElementById("profile").src = "default.jpg";
-    document.getElementById("file-name").textContent = "- Choose Your Profile -";
-});
+document
+    .querySelector("input[type='file']")
+    .addEventListener("change" , e => {
+
+        document
+            .getElementById("profile").src = URL.createObjectURL(e.target.files[0]);
+
+        document
+            .getElementById("file-name").textContent = e.target.files[0].name;
+
+    });
+
+document
+    .querySelector("button[type='reset']")
+    .addEventListener("click" , () => {
+
+        document
+            .getElementById("profile").src = "default.jpg";
+
+        document
+            .getElementById("file-name").textContent = "- Choose Your Profile -";
+
+    });
